24a21f4b2aa689698258afc3b2885a35af3c1656
May Ng <mngmay@gmail.com>
2019-06-21 09:04:19 -0700

    Completed Tabs.js MVP - clicking tabs will produce the related articles

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,7 +5,7 @@ class TabLink {
     // console.log(tabElement);
     // Get the `data-tab` value from this.tabElement and store it here
     this.tabData = this.tabElement.dataset.tab;
-    // console.log(this.tabData); 
+    console.log(this.tabData); 
   
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
@@ -15,7 +15,7 @@ class TabLink {
       this.cards = document.querySelectorAll(".card");
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = document.querySelector(`.card[data-tab='${this.tabData}']`)
+      this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`)
 
     }
   
@@ -54,11 +54,11 @@ class TabLink {
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-    // this.cardElement;
+    this.cardElement = cardElement;
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
-    // this.cardElement;
+    this.cardElement.style.display = "flex";
   }
 
 }
