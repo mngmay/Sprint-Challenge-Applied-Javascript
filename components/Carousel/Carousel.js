e98a12ce954be79e8fcba600095c502de19e97ea
May Ng <mngmay@gmail.com>
2019-06-21 10:37:44 -0700

    Right click works properly in carousel to scroll and display full images

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,45 +1,60 @@
 class Carousel {
-    constructor(element){
-        this.element = element;
-        this.imgs = this.element.querySelectorAll("img");
-        // console.log(this.imgs);
+  constructor(element) {
+    this.element = element;
+    this.imgs = this.element.querySelectorAll("img");
+    // console.log(this.imgs);
 
-        this.rightBtn = this.element.querySelector(".right-button");
-        // console.log(this.rightBtn);
+    this.rightBtn = this.element.querySelector(".right-button");
+    // console.log(this.rightBtn);
 
-        this.leftBtn = this.element.querySelector(".left-button");
-        // console.log(this.leftBtn);
+    this.leftBtn = this.element.querySelector(".left-button");
+    // console.log(this.leftBtn);
 
-        this.index = 0;
-        this.imgs[this.index].style.display = 'flex'; //reveals image based on index
+    this.index = 0;
+    this.imgs[this.index].style.display = "flex"; //reveals image based on index
+    // console.log(this.imgs[this.index]);
 
-        this.rightBtn.addEventListener('click', () => this.moveRight());
-        this.leftBtn.addEventListener('click', () => this.moveLeft());
-    }
+    this.rightBtn.addEventListener("click", () => this.moveRight());
+    this.leftBtn.addEventListener("click", () => this.moveLeft());
+  }
 
-    //methods
+  //methods
 
-    moveRight() {
-     //if button is right add index 
+  moveRight() {
+    //Move one index up for each click, up to the length of the nodelist
+    switch (this.index < Array.from(this.imgs).length - 1) {
+      case true:
         this.index += 1;
-        this.imgs[this.index].style.display = 'flex'; 
-        console.log(this.index);
-        console.log("right click");
+        this.imgs.forEach(img => img.style.display = "none");  //remove all other imgs
+        this.imgs[this.index].style.display = "flex"; //just show selected img
+        break;
+      case false:
+        break;
     }
+    console.log("right click");
+    console.log(this.index);
+  }
 
-    moveLeft() {
-        //if button is left subtract index
+  moveLeft() {
+    //if button is left subtract index until 0
+    switch (this.index > 0) {
+      case true:
         this.index -= 1;
-        this.imgs[this.index].style.display = 'flex'; 
-        console.log("left click");
-       }
+        this.imgs.forEach(img => img.style.display = "none");  //remove all other imgs
+        this.imgs[this.index].style.display = "flex"; //just show selected img
+        break;
+      case false:
+        break;
+    }
+    console.log("left click");
+    console.log(this.index);
+  }
 }
 
 let carousel = document.querySelector(".carousel");
 // console.log(carousel);
 
 carousel = new Carousel(carousel);
-
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
